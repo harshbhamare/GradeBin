b26fdd7e8744afa43f91bb68903aa8b61a108f69
Harsh Bhamare <harshbhamaremit@gmail.com>
2025-08-11 08:42:08 +0530

    vercel config

--- api/index.js
+++ api/index.js
@@ -60,6 +60,8 @@ app.use('/papers', paperViewerRoute);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;
