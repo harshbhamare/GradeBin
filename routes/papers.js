fc46471d37bcfd0451a25f757e8791b2bf3cb857
Harsh Bhamare <harshbhamaremit@gmail.com>
2025-08-12 23:23:36 +0530

    search bug fix

--- routes/papers.js
+++ routes/papers.js
@@ -18,6 +18,34 @@ router.get("/subjects", async (req, res) => {
   }
 });
 
+// router.get('/search', async (req, res) => {
+//   try {
+//     const { department, year, type, subject } = req.query;
+
+//     const query = {};
+
+//     if (department && department !== 'Select Department') {
+//       query.department = new RegExp(`^${department.trim()}$`, 'i');
+//     }
+//     if (year) {
+//       query.year = Number(year);
+//     }
+//     if (type && type !== 'Select Paper Type') {
+//       query.type = new RegExp(`^${type.trim()}$`, 'i');
+//     }
+//     if (subject && subject !== 'Select Subject') {
+//       // Subject is part of name, so do a case-insensitive substring search on 'name'
+//       query.name = { $regex: subject.trim(), $options: 'i' };
+//     }
+
+//     const papers = await QuestionPaper.find(query).lean();
+//     res.json(papers);
+//   } catch (err) {
+//     console.error('Search papers error:', err);
+//     res.status(500).json({ error: 'Server error' });
+//   }
+// });
+
 router.get('/search', async (req, res) => {
   try {
     const { department, year, type, subject } = req.query;
@@ -45,6 +73,7 @@ router.get('/search', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
+
 
 
 module.exports = router;
